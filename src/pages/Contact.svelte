a590785d1db16607bf530c8ba4edbdd95c28dd65
Horlakz <horlakz@protonmail.com>
2022-01-22 23:27:53 +0100

    created contact page

---
+++ src/pages/Contact.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import BaseLayout from "../lib/layouts/BaseLayout.svelte"
+</script>
+
+<BaseLayout>
+    you can contact on my mail or text me on whatsapp on my telephone
+</BaseLayout>
