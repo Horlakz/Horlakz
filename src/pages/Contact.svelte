60b69770074d167e30466815ab07b739996641d8
Horlakz <horlakz@protonmail.com>
2022-02-03 06:19:37 +0100

    adding styles to the contact page

--- src/pages/Contact.svelte
+++ src/pages/Contact.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
-    import BaseLayout from "../lib/layouts/BaseLayout.svelte"
-    import ContactImage from "../assets/images/contact-img.jpeg"
+  import BaseLayout from "../lib/layouts/BaseLayout.svelte";
+  import ContactImage from "../assets/images/contact-img.jpeg";
 </script>
 
 <BaseLayout pglogo={ContactImage}>
-    you can contact on my mail or text me on whatsapp on my telephone
+  <main>
+    <p>you can contact on my mail or text me on whatsapp on my telephone</p>
+  </main>
 </BaseLayout>
+
+<style lang="scss">
+  @use "../assets/sass/main.scss" as *;
+
+  main {
+    width: 100%;
+    // padding-left: 10rem;
+    display: flex;
+    place-content: center;
+    place-items: center;
+
+    p {
+      width: 80%;
+      font-weight: 700;
+      font-size: 1.5rem;
+    }
+  }x``
+</style>
