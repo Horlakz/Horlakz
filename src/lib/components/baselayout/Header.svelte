c4ed7332a4b6b7672c6a3b400890f9108009a318
Horlakz <horlakz@protonmail.com>
2022-03-02 11:30:28 +0100

    styling the header

--- src/lib/components/baselayout/Header.svelte
+++ src/lib/components/baselayout/Header.svelte
@@ -1,24 +1,23 @@
 <!-- script -->
 <script lang="ts">
   import { Link } from "svelte-routing";
-
 </script>
 
 <!-- main content -->
 <header>
   <section>
     <div class="tabs-div">
-      <div></div>
-      <div></div>
+      <div>X</div>
+      <div>-</div>
       <div></div>
     </div>
-    <h1><Link to='/'>horlakz</Link></h1>
+    <h1><Link to="/">olakotan_daniel</Link></h1>
   </section>
   <nav>
     <ul>
-      <li><Link to="/">hello</Link></li>
-      <li><Link to="/about">about</Link></li>
-      <li><Link to="/works">works</Link></li>
+      <li><Link to="/">_hello</Link></li>
+      <li><Link to="/about">_about</Link></li>
+      <li><Link to="/works">_works</Link></li>
     </ul>
   </nav>
 </header>
@@ -30,27 +29,40 @@
   header {
     display: flex;
     flex-direction: column;
-    width: 100%;  
-    
+    width: 100%;
+
     section {
       display: flex;
       justify-content: center;
       place-items: center;
       position: relative;
-      
+      padding: 0.5rem;
+
       .tabs-div {
         display: flex;
         position: absolute;
-        left: .3rem;
+        left: 0.3rem;
 
         div {
-          height: .5rem;
-          width: .5rem;
+          height: 0.8rem;
+          width: 0.8rem;
           border-radius: 999px;
-          margin: 0 .3rem;
+          margin: 0 0.3rem;
+          display: flex;
+          justify-content: center;
+          place-items: center;
+          font-size: .7rem;
+          color: rgba(0, 0, 0, 0);
+          cursor: pointer;
+          @extend .default-transition;
+
+          &:hover {
+            color: white;
+          }
 
           &:first-child {
             background: $pink-color;
+
           }
 
           &:nth-child(2) {
@@ -61,18 +73,30 @@
             background-color: $green-color;
           }
         }
-        
-        h1 {
-          text-align: center;
+      }
+
+      h1 {
+        text-align: center;
+
+        a {
+          color: darken($font-color, 50%);
         }
       }
     }
 
-    nav ul {
-      display: flex;
+    nav {
+      border-top: 2px solid $border-color;
+      border-bottom: 2px solid $border-color;
+      background: darken($bg-color, 4%);
 
-      li {
-        padding: .4rem 1rem;
+      ul {
+        display: flex;
+
+        li {
+          padding: 0.4rem 1.5rem;
+          border-right: 2px solid $border-color;
+          color: lig;
+        }
       }
     }
   }
