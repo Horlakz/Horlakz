82e915f4d3ba0f39d73f4a68df09fb3f107b9975
Horlakz <horlakz@protonmail.com>
2022-03-02 05:12:37 +0100

    added static text for deployment

--- src/lib/layouts/BaseLayout.svelte
+++ src/lib/layouts/BaseLayout.svelte
@@ -42,11 +42,23 @@
 <!-- main content  -->
 <Header />
     <main>
-        <slot />
+        <!-- <slot /> -->
+      Hey! This Website is in the build process and will soon be completed <br>
+      In the meantime you can contact me using the links Below
     </main>
 <Footer />
 
 <!-- component style -->
 <style lang="scss">
   @use "../../assets/sass/main.scss" as *;
+
+  main {
+    display: flex;
+    justify-content: center;
+    place-items: center;
+    text-align: center;
+    font-size: 1.4rem;
+    color: lighten($font-color, 40%);
+    padding-top: 3rem;
+  }
 </style>
